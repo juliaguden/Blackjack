847381fb7a6e7eddb4267b99f9b73c63ae630bf9
juliaguden <julia_guden25@milton.edu>
2024-05-21 09:45:02 -0400

    changed card constructor

--- src/Card.java
+++ src/Card.java
@@ -9,14 +9,78 @@ public class Card {
     //name -> ace, 2, 3...jack, queen, king
     public String name;
 
-    public Card(){
+    //parameters
+    //one for suit
+    //one for name
+    public Card(int pName, int pSuit){
+   // name = pName;
+   // suit = pSuit;
 
+    if(pName == 0){
+       value = 11;
+       name = "Ace";
+    }
+        if(pName == 0){
+            value = 11;
+            name = "Ace";
+        }
+        if(pName == 1){
+            value = 2;
+            name = "Two";
+        }
+        if(pName == 2){
+            value = 3;
+            name = "Three";
+        }
+        if(pName == 3){
+            value = 4;
+            name = "Four";
+        }
+        if(pName == 4){
+            value = 5;
+            name = "Five";
+        }
+        if(pName == 5){
+            value = 6;
+            name = "Six";
+        }
+        if(pName == 6){
+            value = 7;
+            name = "Seven";
+        }
+        if(pName == 7){
+            value = 8;
+            name = "Eight";
+        }
+        if(pName == 8){
+            value = 9;
+            name = "Nine";
+        }
+        if(pName == 9){
+            value = 10;
+            name = "Ten";
+        }
+        if(pName == 10){
+            value = 10;
+            name = "Jack";
+        }
+        if(pName == 11){
+            value = 10;
+            name = "Queen";
+        }
+        if(pName == 12){
+            value = 10;
+            name = "King";
+        }
 
+        if (pSuit == 0) {
+            suit = "Hearts";
+        }
     }
 
-    public void printCard(){
+    public void print(){
 
-        System.out.println( "the"+ name +" of "+ suit +"  is worth "+ value +" points");
+        System.out.println( "the "+ name +" of "+ suit +" is worth "+ value +" points");
     }
 
 }
--- src/Casino.java
+++ src/Casino.java
@@ -1,8 +1,31 @@
 public class Casino {
+
+    public Card aceOfHearts;
+    public Card[] deck;
+
     public static void main(String[] args) {
         Casino X = new Casino();
     }
     public Casino(){
         System.out.println("Welcome to Julia's Casino!");
+
+        deck  = new Card [52];
+
+
+    for(int j = 0; j <= 4; j++) {
+        for (int i = 0; i < 13; i++) {
+            aceOfHearts = new Card(i, j);
+      //      aceOfHearts.print();
+            deck[door] = aceOfHearts;
+        }
     }
+    printDeck();
 }
+public void printDeck(){
+        for(int i = 0; i< deck.length; i++){
+            System.out.println(deck[i]);
+        }
+}
+
+}
+
